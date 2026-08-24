8958004015011e63d10366d9b7f2ca4beb04ed80
Nathan Day <nathan@day.ag>
2026-08-24 15:45:04 +0000

    feat: add OG meta tags for event shared links

--- views.py
+++ views.py
@@ -1,6 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, Request
 from lnbits.core.views.generic import index, index_public
 from lnbits.decorators import check_account_id_exists
+from lnbits.helpers import template_renderer
+from starlette.responses import HTMLResponse
+
+from .crud import get_event
 
 events_generic_router = APIRouter()
 
@@ -11,8 +15,18 @@ events_generic_router.add_api_route(
     dependencies=[Depends(check_account_id_exists)],
 )
 
+
+async def event_public(request: Request, event_id: str) -> HTMLResponse:
+    event = await get_event(event_id)
+    return template_renderer(additional_folders=["events/templates"]).TemplateResponse(
+        request,
+        "event_public.html",
+        {"public": True, "event": event.dict() if event else None},
+    )
+
+
 events_generic_router.add_api_route(
-    "/{event_id}", methods=["GET"], endpoint=index_public
+    "/{event_id}", methods=["GET"], endpoint=event_public
 )
 
 events_generic_router.add_api_route(
